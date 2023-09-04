cfd0329ee6b0872d419997a437dcee5a1690624c
Yaroslav Fedyna <fedynaslava@gmail.com>
2023-09-04 21:46:55 +0300

    message store module added

---
+++ ui/src/store/message.module.js
@@ -0,0 +1,37 @@
+import ChatService from '../services/chat.service'
+const user = JSON.parse(localStorage.getItem('user'));
+
+const initialState = {
+  messageList : {
+    'data' : [],
+  },
+}
+
+export const message = {
+  namespaced: true,
+  state: initialState,
+  actions: {
+    list({ commit }, chatId) {
+      return ChatService.listChatMessages(chatId)
+      .then(
+        pg => {
+          commit('listSuccess', pg);
+          return Promise.resolve(pg);
+        },
+        error => {
+          return Promise.reject(error);
+        }
+      );
+    },
+  },
+  mutations: {
+    listSuccess(state, pg) {
+      state.messageList = pg.data;
+    },
+  },
+  getters : {
+    messageList(state) {
+      return state.messageList
+    },
+  }
+};
